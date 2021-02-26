28acee4ae1b231fe0caaa77f1414c3717cc4f178
gabrielbustos550 <gabrielbustos550@gmail.com>
2021-02-26 10:02:27 -0700

    hw

---
+++ controller/userController.js
@@ -0,0 +1,50 @@
+const router = require('express').Router()
+const db = require('../models')
+
+
+
+
+router.get('/new', (req, res) => {
+    res.render('users/new')
+})
+
+
+
+router.post('/', async (req, res) => {
+    const user = await db.user.create({
+        email:req.body.email,
+        password: req.body.password
+    })
+    res.cookie('userId', user.id)
+    res.redirect('/')
+})
+
+
+router.get('/login', (req, res) => {
+    res.render('users/login')
+})
+
+router.post('/login', async (req, res) => {
+    const user = await db.user.findOne({
+        where: { email: req.body.email}
+    })
+    if(user.password === req.body.password){
+        res.cookie('userId', user.id)
+        res.redirect('/')
+    } else {
+        res.render('users/login')
+    }
+
+})
+
+router.get('/logout', async (req, res) => {
+    res.clearCookie('userId')
+    res.redirect('/')
+})
+
+
+module.exports = router
+
+
+
+
---
+++ server.js
@@ -0,0 +1,49 @@
+const express = require('express')
+const app = express()
+const rowdy = require('rowdy-logger')
+const rowdyResults = rowdy.begin(app)
+const morgan = require('morgan')
+const ejsLayouts = require('express-ejs-layouts')
+const cookieParser = require('cookie-parser')
+
+const models = require('./models')
+
+
+
+app.use(express.urlencoded({ extended: false }))
+
+app.use(morgan('tiny'))
+app.set('view engine', 'ejs')
+app.use(ejsLayouts)
+app.use(cookieParser())
+
+
+app.use('/users', require('./controller/userController'))
+
+
+
+
+app.use(async (req, res, next) => {
+    
+    const user = await models.user.findByPk(req.cookies.userId)
+
+    res.locals.user = user
+
+    next()
+
+})
+
+
+app.get('/', async (req, res, next) => {    
+    res.render('index')
+    
+})
+
+
+
+
+app.listen(3000, () => {
+    console.log('server started!');
+    rowdyResults.print()
+
+})
