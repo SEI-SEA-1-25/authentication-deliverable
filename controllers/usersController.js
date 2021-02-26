d3b25598280224fe08a78db28d7e3a3ccde5b447
LouisaJohnston <louisa.latham.johnston@gmail.com>
2021-02-25 23:01:07 -0800

    had trouble with encryption

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -1,20 +1,75 @@
-// const { Router } = require("express");
+require("dotenv").config();
 const router = require("express").Router()
 const db = require("../models");
-// const cryptojs = require("crypto-js");
-// const bcrypt = require("bcrypt")
+const cryptoJS = require("crypto-js/aes");
+const bcrypt = require("bcrypt")
+
+const SECRET_STRING = process.env.SECRET_STRING;
 
 router.get("/new", (req, res) => {
     res.render("users/new")
+})
+
+router.post("/", async (req, res) => {
+    try {
+        const hashedPassword = bcrypt.hashSync(req.body.password, 10)
+        const user = await db.user.create({
+            email: req.body.email,
+            password: hashedPassword
+        })
+        const encryptedUserId = cryptoJS.encrypt(
+            user.id.toString(), SECRET_STRING)
+        const encryptedUserIdString = encryptedUserId.toString()
+            res.cookie("userId", encryptedUserIdString)
+            res.redirect("/")
+    } catch (err) {
+        console.log(err)
+    }
 })
 
 router.get("/login", (req, res) => {
     res.render("users/login")
 })
 
+router.post("/login", async (req, res) => {
+    try {
+      const user = await db.user.findOne({
+        where: { email: req.body.email },
+      });
+      if (req.body.password === user.password) {
+        const encryptedUserId = cryptoJS.encrypt(
+          user.id.toString(),
+          "super secret string"
+        );
+        const encryptedUserIdString = encryptedUserId.toString();
+  
+        res.cookie("userId", encryptedUserIdString);
+        res.redirect("/");
+      } else {
+        res.render("users/login");
+        console.log("Wrong password");
+      }
+    } catch (err) {
+      console.log(err);
+    }
+  });
+
 router.get("/logout", (req, res) => {
     res.clearCookie("userId")
     res.redirect("/")
 })
+
+// GET profile
+router.get("/profile", (req, res) => {
+    try {
+      if (res.locals.user !== null) {
+        res.render("users/profile");
+      } else {
+        res.redirect("/users/login");
+      }
+    } catch (err) {
+      console.log(err);
+    }
+  });
 
 module.exports = router;
--- server.js
+++ server.js
@@ -1,13 +1,15 @@
+require("dotenv").config();
+
 const express = require("express");
 const app = express();
 const rowdy = require("rowdy-logger");
 const cookieParser = require("cookie-parser");
 const db = require("./models");
-// const cryptojs = require("crypto-js");
+const cryptoJS = require("crypto-js");
 
-// require("dotenv").config();
 
-// const SECRET_STRING = process.env.SECRET_STRING;
+
+const SECRET_STRING = process.env.SECRET_STRING;
 
 // middleware
 const rowdyRes = rowdy.begin(app);
@@ -18,28 +20,34 @@ app.use(require("express-ejs-layouts"));
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 
-// app.use(async (req, res, new) => {
-//     const decryptedUserId = cryptojs.AES.decrypt(req.cookies.userId, SECRET_STRING);
-//     const decryptedUserIdString = decryptedUserId.toString(cryptojs.enc.Utf8);
+// app.use(async (req, res, next) => {
+//     const decryptedUserId = cryptoJS.decrypt(req.cookies.userId, SECRET_STRING);
+//     const decryptedUserIdString = decryptedUserId.toString(cryptoJS.enc.Utf8);
 //     const user = await db.user.findByPk(decryptedUserIdString)
 //     res.locals.user = user;
 //     next();
 // })
 
-app.use(async (req, res, next) => {
-    const user = await db.user.findByPk(req.cookies.userId)
-    res.locals.user = user;
-    next()
-})
 
-app.get("/", async (req, res) => {
-    res.render("index")
+
+app.use(async (req, res, next) => {  
+      const decryptedId = cryptoJS.AES.decrypt(req.cookies.userId, SECRET_STRING)
+      const decryptedIdString = decryptedId.toString(cryptoJS.enc.Utf8)
+        const user = await db.user.findOne({
+          where: {id: decryptedIdString}
+        });
+      res.locals.user = user
+    next()
+  })
+
+app.get("/", (req, res) => {
+  res.render("index");
 });
 
-app.use("/users", require("./controllers/usersController"))
+app.use("/users", require("./controllers/usersController"));
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
-    console.log("server started!");
-    rowdyRes.print();
-})
+  console.log("server started!");
+  rowdyRes.print();
+});
